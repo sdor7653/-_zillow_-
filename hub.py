498c64d2dac2e05724ee1b2ae4a3b8b1031d1fff
Александр <sdor7653@gmail.com>
2022-05-13 18:58:47 +0300

    первый раз

---
+++ hub.py
@@ -0,0 +1,12 @@
+'''Git_Доровских_ИВТ-13'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+table = pd.read_csv('zillow.csv')
+pd.read_csv('zillow.csv',delimiter=',',index_col=False)
+sort = table.sort_values(by='ListPrice')
+table = pd.DataFrame(sort)
+grf = table.groupby('Beds').ListPrice.sum()
+grf.plot.bar(x='Beds)',y='ListPrice',rot=0,color='g')
+plt.show()
