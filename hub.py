3bc58468fc21ec49a3b0758ed6ddadb2e73c1c77
Александр <sdor7653@gmail.com>
2022-05-13 19:08:19 +0300

    удалал sort

--- hub.py
+++ hub.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 table = pd.read_csv('zillow.csv')
 pd.read_csv('zillow.csv',delimiter=',',index_col=False)
 sort = table.sort_values(by='ListPrice')
-table = pd.DataFrame(sort)
+table = pd.DataFrame()
 grf = table.groupby('Beds').ListPrice.sum()
 grf.plot.bar(x='Beds)',y='ListPrice',rot=0,color='g')
 plt.show()
