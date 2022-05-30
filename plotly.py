2e74a506e654815bef9a3d5d332073a3c2498b48
Александр <sdor7653@gmail.com>
2022-05-30 20:52:31 +0300

    8 задание

--- plotly.py
+++ plotly.py
@@ -1,13 +1,21 @@
-'''Графики с Plotly'''
+'''Графики с Plotly_8_работа'''
 
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('../trees.csv')
 pd.read_csv('../trees.csv', delimiter=',', index_col=False)
-fig = px.imshow(df,
+figs = px.imshow(df,
                 labels=dict(x="Long",y="Volume"),
                 x=['Girth', 'Height'],
                 y='Volume'
                 )
+figs.show()
+
+male = pd.read_csv("oscar_age_male.csv")
+female = pd.read_csv("oscar_age_female.csv")
+male['floor'] = 'm'
+female['floor'] = 'w'
+cont = pd.concat([male, female])
+fig = px.scatter(cont, x='Year', y='Age', color='g', title='ID')
+
 fig.show()
